4fa407635f6924d6f0c250fa7906cf0e13749beb
Gaby <gabyospina2111@gmail.com>
2024-03-07 14:39:23 -0500

    commit

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <main className="Imagen">
+        <article className="Portada">
+            <div className="Titulo">
+                <h1>VOGUE</h1>
+            </div>
+            <div className="Contenedor">
+                <div className="Resumen">
+                    <p className="ptitulo">
+                       <em>ARIANA GRANDE</em>
+                    </p>
+                    <p className="ptexto">
+                        <em>THE YEAR THAT CHANGED EVERYTHING</em>
+                    </p>
+                    <p className="ptitulo">Your complete wellness special</p>
+                    <p className="ptexto">
+                        <em>HOW TO DETOX YOUR WARDROBE</em>
+                        </p>
+                    <p className="ptitulo">VOGUE´S 25</p>
+                    <p className="ptexto">Meet the most powerful women in Britain</p>
+                </div>
+                <div className="Subtitulo">
+                    <h2 className="Estilo">
+                    <em>Casual Chic Style</em> 
+                    </h2>
+                    <h3 className="Sugerencia">
+                        <em>WHAT TO WEAR AT EVERY AGE</em>
+                    </h3>
+               </div>
+            </div> 
+       </article>    
+    </main> 
+  )
+}
+
+export default App
